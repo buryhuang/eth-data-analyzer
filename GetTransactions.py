ca9e76cd24f2911e3517e6fa711e9cc6ceb7048f
Bury Huang <burywong.definer@gmail.com>
2019-12-30 09:16:07 -0600

    add batch fetching for compound

--- GetTransactions.py
+++ GetTransactions.py
@@ -11,11 +11,8 @@ import decimal
 import uuid
 import re
 import urllib.parse
-# import msvcrt as m
 import re
-
-# def wait():
-#     m.getch()
+import csv
 
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
@@ -52,36 +49,88 @@ def scroll_down_to_bottom():
             break
         last_height = new_height
 
+def fetch_transaction(driver, tx_hash, outfile):
+    driver.get('https://etherscan.io/tx/%s' % tx_hash)
+
+    html = driver.page_source
+
+    # <textarea readonly="" spellcheck="false" class="form-control bg-light text-secondary text-monospace p-3" rows="8" id="inputdata">Function: enterMarkets(address[] cTokens) ***
+
+    # MethodID: 0xc2998238
+    # [0]:  0000000000000000000000000000000000000000000000000000000000000020
+    # [1]:  0000000000000000000000000000000000000000000000000000000000000008
+    # [2]:  0000000000000000000000006c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e
+    # [3]:  0000000000000000000000005d3a536e4d6dbd6114cc1ead35777bab948e3643
+    # [4]:  0000000000000000000000004ddc2d193948926d02f9b1fe9e1daa0718270ed5
+    # [5]:  000000000000000000000000158079ee67fce2f58472a96584a73c7ab9ac95c1
+    # [6]:  000000000000000000000000f5dce57282a584d2746faf1593d3121fcac444dc
+    # [7]:  00000000000000000000000039aa39c021dfbae8fac545936693ac917d5e7563
+    # [8]:  000000000000000000000000c11b1268c1a384e55c48c2391d8d480264a3a7f4
+    # [9]:  000000000000000000000000b3319f5d18bc0d84dd1b4825dcde5d5f7266d407</textarea>
+
+    m = re.search('<textarea.*?id="inputdata">.*?Function: (.*?)(\(.*?\)).*?MethodID: (.*?)(\[.*?)</textarea>', html, re.DOTALL)
+    if m is not None:
+        functionName = m.group(1).rstrip()
+        arguments =  m.group(2).rstrip()
+        methodId = m.group(3).rstrip()
+        parameters_raw = m.group(4) + '\n'
+        parameters = re.findall('\[\d+\]: (.*?)\n', parameters_raw, re.DOTALL)
+        outfile.write("%s,%s,%s,%s\n" % (tx_hash, functionName, methodId, ','.join(parameters)))
+    else:
+        outfile.write("%s\n" % (tx_hash)) # Record it has been processed
+
+def fetch_transactions(driver, tx_hash_list, out_filename):
+    with open(out_filename, "a") as outfile:
+        process_count = 1
+        for tx in tx_hash_list:
+            fetch_transaction(driver, tx, outfile)
+            print("Finished [%d] %s\n" % (process_count, tx))
+            process_count += 1
+
 # Main
-chromedriver = "./chromedriver"
-os.environ["webdriver.chrome.driver"] = chromedriver
-driver = webdriver.Chrome(chromedriver)
-driver.get('https://etherscan.io/tx/0x452d3bc7caddd5ce1e26e48152edb5ee0bf0fe31b8f94662bcf31bfdc2fee378')
 
-html = driver.page_source
+tx_list = []
+existing_tx_map = {}
+data_folder = "raw_data/compound"
 
-# <textarea readonly="" spellcheck="false" class="form-control bg-light text-secondary text-monospace p-3" rows="8" id="inputdata">Function: enterMarkets(address[] cTokens) ***
+input_filename = '%s/Comptroller-0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b.csv' % data_folder
+output_filename = '%s/compound_function_calls.csv' % data_folder
 
-# MethodID: 0xc2998238
-# [0]:  0000000000000000000000000000000000000000000000000000000000000020
-# [1]:  0000000000000000000000000000000000000000000000000000000000000008
-# [2]:  0000000000000000000000006c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e
-# [3]:  0000000000000000000000005d3a536e4d6dbd6114cc1ead35777bab948e3643
-# [4]:  0000000000000000000000004ddc2d193948926d02f9b1fe9e1daa0718270ed5
-# [5]:  000000000000000000000000158079ee67fce2f58472a96584a73c7ab9ac95c1
-# [6]:  000000000000000000000000f5dce57282a584d2746faf1593d3121fcac444dc
-# [7]:  00000000000000000000000039aa39c021dfbae8fac545936693ac917d5e7563
-# [8]:  000000000000000000000000c11b1268c1a384e55c48c2391d8d480264a3a7f4
-# [9]:  000000000000000000000000b3319f5d18bc0d84dd1b4825dcde5d5f7266d407</textarea>
+# get list of already fetched tx
+with open(output_filename, 'r') as output_csv_file:
+    csv_reader = csv.reader(output_csv_file, delimiter=',')
+    for row in csv_reader:
+        tx_hash = row[0]
+        existing_tx_map[tx_hash] = True
 
-m = re.search('<textarea.*?id="inputdata">(.*?)</textarea>', html, re.DOTALL)
-if m is not None:
-    print(m.group(0))
+# process new records
+line_limit = 2500
+with open(input_filename) as input_csv_file:
+    csv_reader = csv.reader(input_csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count > line_limit:
+            break
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'fetching \t{row[0]}')
+            tx = row[0]
+            # fetch_transaction(driver, tx)
+            if tx not in existing_tx_map:
+                tx_list.append(tx)
+            line_count += 1
+    print(f'Processed {line_count} lines. New records found {len(tx_list)}')
 
-# print('Waiting for user login. When done, press any key to continue...')
-# wait()
+# Go Fetch
+if len(tx_list) > 0:
+    chromedriver = "./chromedriver"
+    os.environ["webdriver.chrome.driver"] = chromedriver
+    driver = webdriver.Chrome(chromedriver)
 
-# scroll_down_to_bottom()
+    # fetch_transaction(driver, '0x452d3bc7caddd5ce1e26e48152edb5ee0bf0fe31b8f94662bcf31bfdc2fee378')
+    fetch_transactions(driver, tx_list, '%s/compound_function_calls.csv' % data_folder)
 
-driver.quit()
+    driver.quit()
 
